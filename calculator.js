c440c91792742bd5920c495fa705f9fa97b0141b
Erik <erikcat06@gmail.com>
2019-03-27 10:59:17 -0600

    x^4 botón

--- calculator.js
+++ calculator.js
@@ -4,9 +4,17 @@ var botones = document.getElementById('button-addon4');
 
 botones.innerHTML = `
 <button class="btn btn-danger" id="cube" >x^3</button>
+<button class="btn btn-warning" id="x4" >x^4</button>
 `;
 var cube = document.getElementById('cube');
+var x4 = document.getElementById('x4');
+
 cube.addEventListener('click', cube =>
 {
       resultado.innerHTML = `<h1>El resultado de ${num.value} <span class="text-danger"><strong>x^3</strong></span> es: <span class="alert alert-success">${Math.pow(num.value, 3)}</span></h1>`;
+});
+
+x4.addEventListener('click', x4 =>
+{
+      resultado.innerHTML = `<h1>El resultado de ${num.value} <span class="text-warning"><strong>x^4</strong></span> es: <span class="alert alert-success">${Math.pow(num.value, 4)}</span></h1>`;
 });
